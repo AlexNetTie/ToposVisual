eb4b3290e4e52eaf192a36b57e3b3d16977c7709
AlexNetTie <sanya.pletniov3196@gmail.com>
2026-04-08 01:48:42 +0300

    Поправил форматирование у файла

--- Topos/src/main/java/Lost/Architect/Development/Soulvers/Metrics/MetricGradient.java
+++ Topos/src/main/java/Lost/Architect/Development/Soulvers/Metrics/MetricGradient.java
@@ -12,11 +12,14 @@ public class MetricGradient implements ElegantMetric<Double,List<Double>> {
     @Override
     @Invariant("Расчет градиента ∂φ/∂l")
     @Parameters({"parameters.get(0) - current value",
-            "parameters.get(1) - next value",
-            "parameters.get(2) - ∂l"})
+                 "parameters.get(1) - next value",
+                 "parameters.get(2) - ∂l"})
     public Double solvedMetric(ElegantFunction el, List<Double> parameters) {
-        return el.merge(el.calculation(parameters.get(1),parameters.get(0)*(-1),1), // φ[1] - φ[0]
+        return el.merge(
+                el.calculation(parameters.get(1), //φ[1]
+                               parameters.get(0)*(-1),1), // φ[0]
                 el.merge(1, parameters.get(2),-1), // 1/∂l
-                1);
+                1
+        );
     }
 }
